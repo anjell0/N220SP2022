29ab96e0dd037236bf06d459f25682f35a171f2e
anjell0 <aka2@iu.edu>
2022-02-03 19:52:44 -0500

    More comments

--- labs/lab3/redremover/js/app.js
+++ labs/lab3/redremover/js/app.js
@@ -2,15 +2,14 @@
 //February 3, 2022
 //N220-20816
 
-function draw() {
-    
-    noRed = removeRed(color(170, 200, 150));
-    fill(noRed)
-    circle(50, 50, 50);
-}
+function draw() {                               //Lines 5-9 define the variable "noRed" to
+    noRed = removeRed(color(170, 200, 150));    //call the function "removeRed" and its 
+    fill(noRed)                                 //color argument to fill a circle that has
+    circle(50, 50, 50);                         //removed the red value from a color
+} 
 
-function removeRed(c) {
-    console.log(c)
-    c.setRed(0)
-    return c
+function removeRed(c) {                         //Lines 11-15 create a function that takes
+    console.log(c)                              //an argument "c" and defines it to set a
+    c.setRed(0)                                 //red value to 0 and then return the result
+    return c                                    //as a value when called on
 }
