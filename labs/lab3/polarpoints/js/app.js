2a7dabfd475a8cfea22a697a256f720d3eecc74a
anjell0 <aka2@iu.edu>
2022-02-03 19:47:35 -0500

    Added comments

--- labs/lab3/polarpoints/js/app.js
+++ labs/lab3/polarpoints/js/app.js
@@ -3,20 +3,20 @@
 //N220-20816
 
 
-function setup() {
-    createCanvas(800, 600);
-    background(50);
+function setup() {                  //Lines 6-9 create a setup function that creates
+    createCanvas(800, 600);         //a canvas 800x600 pixels and sets the background
+    background(50);                 //to dark grey
 }
 
-function draw() {
-    res = new polarPoint(10);
-    translate(100, 100);
-    ellipse(res.x, res.y, 10);
+function draw() {                   //Lines 11-15 draws an ellipse with an origin
+    res = new polarPoint(10);       //translated to a new coordinate of (100, 100)
+    translate(100, 100);            //while calling the values of the function named
+    ellipse(res.x, res.y, 10);      //"polarPoint"
 }
 
-function polarPoint(r) {
-    var x = r * Math.sin(mouseX);
-    var y = r * Math.cos(mouseX);
-    return createVector(x,y);
-}
+function polarPoint(r) {            //Lines 17-21 create a funtion that will cause a
+    var x = r * Math.sin(mouseX);   //shape to be drawn with an x and y coordinate
+    var y = r * Math.cos(mouseX);   //that reflect the sine and cosine of the mouse's
+    return createVector(x,y);       //X-coordinate multplied by the functions argument
+}                                   //"r" which is then defined in the draw function
 
