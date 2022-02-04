0cfc4a550a326a104ce6453be5121510b98ce2e2
anjell0 <aka2@iu.edu>
2022-02-03 19:56:44 -0500

    Cleaned up comments

--- labs/lab3/drawingdrawer/js/app.js
+++ labs/lab3/drawingdrawer/js/app.js
@@ -2,34 +2,24 @@
 //February 3, 2022
 //N220-20816
 
-function setup() {
-    createCanvas(800, 600)  //creates a canvas 800px wide and 600px tall
-    background(0)  //makes a black background
+function setup() {                          //Lines 5-8 create a canvas 800px wide and 600px tall
+    createCanvas(800, 600)                  //and makes a black background
+    background(0)                           
 }
 
-function draw() {
-    Vehicle(mouseX, mouseY);  //draws a "car" to the mouse location
+function draw() {                           //Lines 10-12 draw a "car" to the mouse location
+    Vehicle(mouseX, mouseY);                
 }
 
-function Vehicle(x,y) {  //Lines 13 - 45 create the function which will create a shape at a certain location when called
-
-fill(170, 0, 0)
-
-rect(x - 100, y - 25, 200, 50);
-
-ellipse(x + 62.5, y + 25, 75);
-
-ellipse(x - 62.5, y + 25, 75);
-
-quad(x - 90, y - 25, x - 65, y - 75, x + 15, y - 75, x + 75, y - 25)
-
-fill(0)
-
-triangle(x + 15, y - 65, x + 15, y - 25, x + 65, y - 25)
-
-triangle(x - 80, y - 25, x - 65, y - 65, x - 65, y - 25)
-
-rect(x - 10, y - 65, 25, 40)
-
-rect(x - 60, y - 65, 45, 40)
+function Vehicle(x,y) {                     //Lines 14-25 create the function which will 
+    fill(170, 0, 0)                         //create an image at a certain location when
+    rect(x - 100, y - 25, 200, 50);         //called
+    ellipse(x + 62.5, y + 25, 75);
+    ellipse(x - 62.5, y + 25, 75);
+    quad(x - 90, y - 25, x - 65, y - 75, x + 15, y - 75, x + 75, y - 25)
+    fill(0)
+    triangle(x + 15, y - 65, x + 15, y - 25, x + 65, y - 25)
+    triangle(x - 80, y - 25, x - 65, y - 65, x - 65, y - 25)
+    rect(x - 10, y - 65, 25, 40)
+    rect(x - 60, y - 65, 45, 40)
 }
